b2d53afa3af96425b7726fa802c92c7c7ef0127c
Samuel Chima <samuelnkpado@gmail.com>
2026-03-26 18:57:11 +0100

    Set production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL || "https://valuebridge-api.onrender.com",
+  headers: { "Content-Type": "application/json" },
 });
 
 // Automatically attach token to every request
